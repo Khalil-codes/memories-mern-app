6c9fe73198ab58b3e450cdc9d89cf9d1701b0cee
khalil-codes <khalilpati55@gmail.com>
2022-03-07 02:59:00 +0530

    Setup Routes, and port listeners

--- server/dist/js/app.js
+++ server/dist/js/app.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("Hello World how");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+// dotenv config in typescript
+dotenv_1.default.config({ path: `${__dirname}/../../.env` });
+// Main Imports
+const express_1 = __importDefault(require("express"));
+// Start
+const app = (0, express_1.default)();
+// Middlewares
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,24 @@
 import dotenv from "dotenv";
 // dotenv config in typescript
 dotenv.config({ path: `${__dirname}/../../.env` });
-
+const PORT: string | number = process.env.PORT || 5000;
 // Main Imports
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import morgan from "morgan";
 
 // Start
 const app: Express = express();
 
 // Middlewares
+app.use(cors());
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// Routes Middleware
+app.get("/api/memories", (req: Request, res: Response) => {
+    res.status(200).json({ message: "Hello World" });
+});
+// Listen
+app.listen(PORT, () => console.log(`Server Started on port ${PORT}`));
