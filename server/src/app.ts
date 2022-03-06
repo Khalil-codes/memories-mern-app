fc69400224616ece66ce7b91e3622926a57f4837
khalil-codes <khalilpati55@gmail.com>
2022-03-07 02:38:57 +0530

    Configured .env

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,13 @@
+import dotenv from "dotenv";
+// dotenv config in typescript
+dotenv.config({ path: `${__dirname}/../../.env` });
 
+// Main Imports
+import express, { Express } from "express";
+
+// Start
+const app: Express = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
