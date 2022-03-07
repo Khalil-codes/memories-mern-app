e9a74381fb90ff9b1461503dbdd77411bbe55940
khalil-codes <khalilpati55@gmail.com>
2022-03-07 13:17:06 +0530

    Added Post Interface

---
+++ server/src/types/posts.d.ts
@@ -0,0 +1,10 @@
+export interface IPost {
+    title: string;
+    message: string;
+    creator: string;
+    tags: string[];
+    selectedFile: string;
+    likeCount: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
