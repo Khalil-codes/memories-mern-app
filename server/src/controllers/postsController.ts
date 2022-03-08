8b41b9314049f23afd1550b8c6dd1a4ebf2c7506
khalil-codes <khalilpati55@gmail.com>
2022-03-08 15:32:38 +0530

    Added updatePost and deletePost controllers

--- server/src/controllers/postsController.ts
+++ server/src/controllers/postsController.ts
@@ -33,3 +33,46 @@ export const createPost = async (req: Request, res: Response) => {
         res.status(404).json({ status: "fail", message: error.message });
     }
 };
+
+export const updatePost = async (req: Request, res: Response) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        const updatedPostData = {
+            ...req.body,
+            tags: req.body.tags.split(",").map((tag: string) => tag.trim()),
+        };
+        if (!post) {
+            res.status(404).json({ status: "fail", message: "Post not Found" });
+        }
+        const updatedPost = await Post.findByIdAndUpdate(
+            req.params.id,
+            updatedPostData
+        );
+        res.status(201).json({
+            status: "success",
+            data: {
+                post: updatedPost,
+            },
+        });
+    } catch (error: any) {
+        res.status(404).json({ status: "fail", message: error.message });
+    }
+};
+
+export const deletePost = async (req: Request, res: Response) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            res.status(404).json({ status: "fail", message: "Post not Found" });
+        }
+        await Post.findByIdAndDelete(req.params.id);
+        res.status(201).json({
+            status: "success",
+            data: {
+                _id: req.params.id,
+            },
+        });
+    } catch (error: any) {
+        res.status(404).json({ status: "fail", message: error.message });
+    }
+};
--- server/src/routes/postsRoutes.ts
+++ server/src/routes/postsRoutes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { createPost, getPosts } from "../controllers/postsController";
+import {
+    createPost,
+    deletePost,
+    getPosts,
+    updatePost,
+} from "../controllers/postsController";
 
 const router: Router = Router();
 
 router.route("/").get(getPosts).post(createPost);
+router.route("/:id").patch(updatePost).delete(deletePost);
 
 export default router;
