f296dd3d095957ea97d5c6305ec29e89b5aaa52a
khalil-codes <khalilpati55@gmail.com>
2022-03-08 18:42:41 +0530

    Fixed a Bug

--- client/src/components/Posts/Post/PostOverlay.tsx
+++ client/src/components/Posts/Post/PostOverlay.tsx
@@ -3,12 +3,11 @@ import React, { FC } from "react";
 import { Card } from "react-bootstrap";
 
 type Props = {
-    title: string;
     creator: string;
     createdAt: Date;
 };
 
-const PostOverlay: FC<Props> = ({ title, creator, createdAt }) => {
+const PostOverlay: FC<Props> = ({ creator, createdAt }) => {
     return (
         <Card.ImgOverlay>
             <Card.Title
@@ -16,7 +15,7 @@ const PostOverlay: FC<Props> = ({ title, creator, createdAt }) => {
                     color: "#fff",
                     fontWeight: 400,
                 }}>
-                {title}
+                {creator}
             </Card.Title>
             <Card.Subtitle
                 style={{
@@ -24,7 +23,7 @@ const PostOverlay: FC<Props> = ({ title, creator, createdAt }) => {
                     fontWeight: 400,
                     fontSize: "0.9rem",
                 }}>
-                -{creator}, {moment(createdAt).fromNow()}
+                {moment(createdAt).fromNow()}
             </Card.Subtitle>
         </Card.ImgOverlay>
     );
