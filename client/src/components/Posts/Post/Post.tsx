e2f649bc1dedcc60bdbf8958cccaa949c5f650a8
khalil-codes <khalilpati55@gmail.com>
2022-03-08 15:31:43 +0530

    Added deletePost function using redux-thunk

--- client/src/components/Posts/Post/Post.tsx
+++ client/src/components/Posts/Post/Post.tsx
@@ -1,70 +1,38 @@
-import moment from "moment";
 import React, { FC } from "react";
-import { Badge, Button, Card, Col, Row } from "react-bootstrap";
-import { FaTrash, FaThumbsUp, FaEdit } from "react-icons/fa";
+import { Card } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { deletePost } from "../../../redux/postSlice";
 import { IPost } from "../../../types";
+import PostAction from "./PostAction";
+import PostBadges from "./PostBadges";
+import PostOverlay from "./PostOverlay";
 
 interface Props {
     post: IPost;
 }
 
-const getRandomBadge = () => {
-    const bg = ["primary", "secondary", "success", "dark"];
-    return bg[Math.trunc(Math.random() * bg.length) + 1];
-};
-
 const Post: FC<Props> = ({ post }) => {
+    const dispatch = useDispatch();
     const handleDelete = () => {
         console.log(post._id);
+        try {
+            dispatch(deletePost(post._id));
+        } catch (error: any) {
+            console.log(error.message, "here");
+        }
     };
     return (
         <Card>
             <Card.Img variant="top" src={post.selectedFile} />
-            <Card.ImgOverlay>
-                <Card.Title
-                    style={{
-                        color: "#fff",
-                        fontWeight: 400,
-                    }}>
-                    {post.title}
-                </Card.Title>
-                <Card.Subtitle
-                    style={{
-                        color: "#fff",
-                        fontWeight: 400,
-                        fontSize: "0.9rem",
-                    }}>
-                    -{post.creator}, {moment(post.createdAt).fromNow()}
-                </Card.Subtitle>
-            </Card.ImgOverlay>
-            <Card.Body>
-                <div className="mb-2">
-                    {post.tags.map((tag) => (
-                        <Badge
-                            key={tag}
-                            bg={getRandomBadge()}
-                            style={{ opacity: 0.7 }}
-                            className="me-1">
-                            # {tag}
-                        </Badge>
-                    ))}
-                </div>
+            <PostOverlay
+                title={post.title}
+                creator={post.creator}
+                createdAt={post.createdAt}
+            />
+            <Card.Body style={{ zIndex: 10 }}>
+                <PostBadges tags={post.tags} />
                 <Card.Text>{post.message}</Card.Text>
-                <Row>
-                    <Col xs={6}>
-                        <Button variant="primary">
-                            <FaThumbsUp /> 0
-                        </Button>
-                    </Col>
-                    <Col xs={6} className="text-end">
-                        <Button variant="warning" className="me-1">
-                            <FaEdit />
-                        </Button>
-                        <Button variant="danger" onClick={handleDelete}>
-                            <FaTrash />
-                        </Button>
-                    </Col>
-                </Row>
+                <PostAction handleDelete={handleDelete} />
             </Card.Body>
         </Card>
     );
