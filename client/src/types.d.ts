c6841bf2c4c19c9f3eb31212434aa63ba56bcb2e
khalil-codes <khalilpati55@gmail.com>
2022-03-08 11:34:47 +0530

    Declared types of post

---
+++ client/src/types.d.ts
@@ -0,0 +1,23 @@
+export interface IPost {
+    id: string;
+    title: string;
+    message: string;
+    creator: string;
+    tags: string[];
+    selectedFile: string;
+    likeCount: number;
+    createdAt: Date;
+    updatedAt: Date;
+}
+export interface IPostClient {
+    title: string;
+    message: string;
+    creator: string;
+    tags: string;
+    selectedFile: string;
+}
+export interface IReduxInitialState {
+    posts?: IPost[];
+    loading: boolean;
+    error: boolean;
+}
--- server/src/types/posts.d.ts
+++ server/src/types/posts.d.ts
@@ -1,4 +1,5 @@
 export interface IPost {
+    id: string;
     title: string;
     message: string;
     creator: string;
