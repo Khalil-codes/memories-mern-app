d8016d1d73e92d7fad0e4f8631a7925c2a0215b1
khalil-codes <khalilpati55@gmail.com>
2022-03-08 11:38:16 +0530

    Derived API definition using axios

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,8 +1,16 @@
 import axios from "axios";
 
 const url = "http://localhost:5000/api/posts";
+
+// Get all Post
 export const getPosts = () => axios.get(url);
+
+// Create Post
 export const createPost = (postData: any) => axios.post(url, postData);
+
+// Update Post
 export const updatePost = (id: string, updatedPostData: any) =>
     axios.patch(`${url}/${id}`, updatedPostData);
+
+// Delete Post
 export const deletePost = (id: string) => axios.delete(`${url}/${id}`);
