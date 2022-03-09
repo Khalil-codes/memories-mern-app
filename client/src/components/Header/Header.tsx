94145e43f7fd81aa7c916fb236a1cce097bc20ad
khalil-codes <khalilpati55@gmail.com>
2022-03-09 14:29:22 +0530

    Added user-avatar field

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,10 +1,12 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { LinkContainer } from "react-router-bootstrap";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import Avatar from "react-avatar";
 
 type Props = {};
 
 const Header: FC<Props> = (props) => {
+    const [user, setUser] = useState(true);
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
@@ -16,12 +18,41 @@ const Header: FC<Props> = (props) => {
                 <Navbar.Toggle aria-controls="basic-navbar" />
                 <Navbar.Collapse id="basic-navbar">
                     <Nav className="ms-auto">
-                        <LinkContainer to="/login">
-                            <Nav.Link>Login</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to="/register">
-                            <Nav.Link>Register</Nav.Link>
-                        </LinkContainer>
+                        {user ? (
+                            <>
+                                <div className="user-info me-4 d-flex justify-content-center align-items-center h-100">
+                                    <Avatar
+                                        name="Khalil Patiwala"
+                                        size="30"
+                                        round={true}
+                                        textSizeRatio={1.5}
+                                    />
+                                    <Navbar.Text className="ms-2">
+                                        Khalil Patiwala
+                                    </Navbar.Text>
+                                </div>
+                                <Nav.Link
+                                    className="btn btn-light text-dark"
+                                    onClick={() => setUser(false)}>
+                                    Logout
+                                </Nav.Link>
+                            </>
+                        ) : (
+                            <>
+                                <LinkContainer to="/login">
+                                    <Nav.Link>Login</Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to="/register">
+                                    <Nav.Link>Register</Nav.Link>
+                                </LinkContainer>
+
+                                <Nav.Link
+                                    className="btn btn-light text-dark"
+                                    onClick={() => setUser(true)}>
+                                    Login
+                                </Nav.Link>
+                            </>
+                        )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
