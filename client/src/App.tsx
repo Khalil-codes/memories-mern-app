66e844f66dedd06608f172f651c1d82e867e54fb
khalil-codes <khalilpati55@gmail.com>
2022-03-09 16:10:45 +0530

    Added Protected Routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,34 @@
 import React, { FC } from "react";
 import { Container } from "react-bootstrap";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { useAuth } from "./redux/store";
 
 const App: FC = () => {
+    const { user } = useAuth();
+
     return (
         <>
             <Header />
             <Container>
                 <Routes>
-                    <Route path="/" element={<Dashboard />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/register" element={<Register />} />
+                    <Route
+                        path="/"
+                        element={
+                            user ? <Dashboard /> : <Navigate to="/login" />
+                        }
+                    />
+                    <Route
+                        path="/login"
+                        element={!user ? <Login /> : <Navigate to="/" />}
+                    />
+                    <Route
+                        path="/register"
+                        element={!user ? <Register /> : <Navigate to="/" />}
+                    />
                 </Routes>
             </Container>
         </>
