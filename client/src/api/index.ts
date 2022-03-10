3c749f39e31f2cd74cb073e167d82a300a1c22a7
khalil-codes <khalilpati55@gmail.com>
2022-03-10 12:01:44 +0530

    Added Auth Services in API

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,22 +1,32 @@
 import axios from "axios";
-import { IPostClient } from "../types";
+import { ICred, IPostClient, IUserData } from "../types";
 
-const url = "http://localhost:5000/api/posts";
+const postsUrl = "http://localhost:5000/api/posts";
+const authUrl = "http://localhost:5000/api/user";
 
 // Get all Post
-export const getPosts = () => axios.get(url);
+export const getPosts = () => axios.get(postsUrl);
 
 // Create Post
-export const createPost = (postData: IPostClient) => axios.post(url, postData);
+export const createPost = (postData: IPostClient) =>
+    axios.post(postsUrl, postData);
 
 // Update Post
 export const updatePost = (id: string, updatedPostData: IPostClient) => {
     console.log(id, updatedPostData);
-    return axios.patch(`${url}/${id}`, updatedPostData);
+    return axios.patch(`${postsUrl}/${id}`, updatedPostData);
 };
 
 // Delete Post
-export const deletePost = (id: string) => axios.delete(`${url}/${id}`);
+export const deletePost = (id: string) => axios.delete(`${postsUrl}/${id}`);
 
 // Like Post
-export const likePost = (id: string) => axios.patch(`${url}/${id}/like`);
+export const likePost = (id: string) => axios.patch(`${postsUrl}/${id}/like`);
+
+// User Authentication
+export const loginUser = (credentials: ICred) =>
+    axios.post(`${authUrl}/login`, credentials);
+
+// Register User
+export const registerUser = (userData: IUserData) =>
+    axios.post(authUrl, userData);
