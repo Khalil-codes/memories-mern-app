64afabd355054dba08a89ac57fb7c7e02f7f284b
khalil-codes <khalilpati55@gmail.com>
2022-03-14 13:40:44 +0530

    Added Hook to check JWT expiry

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,22 @@
-import React, { FC } from "react";
+import jwtDecode from "jwt-decode";
+import React, { FC, useEffect } from "react";
 import { Container } from "react-bootstrap";
+import { useDispatch } from "react-redux";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import { logoutUser } from "./redux/authSlice";
 import { useAuth } from "./redux/store";
 
 const App: FC = () => {
     const { user } = useAuth();
-
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const decoded: any = jwtDecode(user?.token as string);
+        if (decoded?.exp * 1000 < new Date().getTime()) dispatch(logoutUser());
+    }, []);
     return (
         <>
             <Header />
